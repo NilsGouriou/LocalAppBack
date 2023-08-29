8220ed9515196c435e64e3e44cfeebfff3a9a8cf
Nils <nils.gouriou@gmail.com>
2023-08-29 10:15:57 +0200

    Added : initial base architecture & security module

---
+++ src/main/java/fr/eql/ai114/ngouriou/localappback/security/SecurityConfigurer.java
@@ -0,0 +1,67 @@
+package fr.eql.ai114.ngouriou.localappback.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+
+    @Override @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Enable CORS and disable CSRF
+        http = http.cors().and().csrf().disable();
+
+        // Set session management to stateless
+        http = http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and();
+
+        // Set unauthorized requests exception handler
+        http = http
+            .exceptionHandling()
+            .authenticationEntryPoint(new SecurityEntryPoint())
+            .and();
+
+        // Set permissions on endpoints
+        http
+            .authorizeRequests()
+            // Our public endpoints
+            .antMatchers("/font-awesome-4.7.0/**/**").permitAll()
+            .antMatchers("/").permitAll()
+            .antMatchers("/**/*.html").permitAll()
+            .antMatchers("/**/*.css").permitAll()
+            .antMatchers("/**/*.js").permitAll()
+            .antMatchers("/**/*.jpg").permitAll()
+            .antMatchers("/api/security/**").permitAll()
+            .antMatchers("/api/glossary/**").permitAll()
+
+            // Our private endpoints
+            .anyRequest().authenticated();
+
+        http
+            .addFilterBefore(securityFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public SecurityFilter securityFilter() {
+        return new SecurityFilter();
+    }
+
+
+}
---
+++ src/main/java/fr/eql/ai114/ngouriou/localappback/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package fr.eql.ai114.ngouriou.localappback.security;
+
+import fr.eql.akatz.cats.club.back.spring.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+	/** Injected by the setter */
+	private SecurityService securityService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+        try {
+            String token = extractTokenFromHeader(request);
+            UserDetails user = securityService.getOwnerFromJsonWebToken(token);
+            setPrincipalInSecurityContext(user);
+        } catch (Exception e) {
+            logger.info("Trying parse token but failed");
+        }
+        filterChain.doFilter(request, response);
+	}
+
+	private String extractTokenFromHeader(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (bearerToken != null) {
+			return bearerToken.substring(7, bearerToken.length());
+		}
+		return null;
+	}
+
+	private void setPrincipalInSecurityContext(UserDetails user) {
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(token);
+	}
+
+	/// Setters ///
+	@Autowired
+	public void setConnectionService(SecurityService securityService) {
+		this.securityService = securityService;
+	}
+}
